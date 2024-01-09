4d12f061a9bfcb9d0bae143b25eb1eb0e885377d
Shaikdilnawaz <154993930+Shaikdilnawaz27@users.noreply.github.com>
2024-01-09 16:23:46 +0530

    Create exception withthrows keyword.java

---
+++ exception withthrows keyword.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Test
+{
+public static void main(String[] args ) throws Interruptedexception
+{
+System.out.println("C language");
+Thread.sleep(3000);
+System.out.println("java");
+Thread.sleep(2000);
+System.out.println("python");
+}
+}
