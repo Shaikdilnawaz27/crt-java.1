bb61b50cfb682939ff2cc9caab04139250753a2c
Shaikdilnawaz <154993930+Shaikdilnawaz27@users.noreply.github.com>
2024-01-09 16:24:49 +0530

    Create checked exception with try catch block.java

---
+++ checked exception with try catch block.java
@@ -0,0 +1,25 @@
+class Test
+{
+public static void main(String[] args)
+{
+System.out.println("C Language");
+try
+{
+Thread.sleep(3000);
+}
+catch(InterruptedException e)
+{
+System.out.println(e);
+}
+System.out.println("java");
+try
+{
+Thread.sleep(2000);
+}
+catch(InterruptedException e)
+{
+System.out.println(e);
+{
+System.out.println("python");
+}
+}
